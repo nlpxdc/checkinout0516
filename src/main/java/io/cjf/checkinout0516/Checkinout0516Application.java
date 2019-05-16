e9351dd4bc8d08f010194e9c4d4f9df7fb36ed2d
chenjiefei <nlpx_dc@hotmail.com>
2019-05-16 14:08:33 +0800

    first init

---
+++ src/main/java/io/cjf/checkinout0516/Checkinout0516Application.java
@@ -0,0 +1,13 @@
+package io.cjf.checkinout0516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkinout0516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Checkinout0516Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/cjf/checkinout0516/Checkinout0516ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cjf.checkinout0516;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Checkinout0516ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
