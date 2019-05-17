c2bd24f0d26731a8d82c4ba08d8b529f2a09e184
chenjiefei <nlpx_dc@hotmail.com>
2019-05-17 09:01:32 +0800

    draft check in logic

--- src/main/java/io/cjf/checkinout0516/controller/WechatMPController.java
+++ src/main/java/io/cjf/checkinout0516/controller/WechatMPController.java
@@ -1,23 +1,32 @@
 package io.cjf.checkinout0516.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.grum.geocalc.Coordinate;
 import com.grum.geocalc.EarthCalc;
 import com.grum.geocalc.Point;
+import io.cjf.checkinout0516.dao.CheckInOutMapper;
+import io.cjf.checkinout0516.enumeration.CheckType;
+import io.cjf.checkinout0516.po.CheckInOut;
 import io.cjf.checkinout0516.vo.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.soap.Addressing;
+import java.util.Date;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/wechatmp")
+@EnableAutoConfiguration
 public class WechatMPController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -29,6 +38,9 @@ public class WechatMPController {
 
     @Value("${check.longitude}")
     private Double checkLongitude;
+
+    @Autowired
+    private CheckInOutMapper checkInOutMapper;
 
     @PostMapping(value = "/receive",produces = MediaType.APPLICATION_XML_VALUE)
     public JSONObject receive(@RequestBody JSONObject message){
@@ -43,6 +55,8 @@ public class WechatMPController {
                     String openid = message.getString("FromUserName");
                     Position position = openidPosition.get(openid);
 
+                    //todo check if position null
+
                     Coordinate lat = Coordinate.fromDegrees(checkLatitude);
                     Coordinate lng = Coordinate.fromDegrees(checkLongitude);
                     Point checkPosition = Point.at(lat, lng);
@@ -56,6 +70,28 @@ public class WechatMPController {
                         //todo return message to wechat
                     }
 
+                    CheckInOut checkInOut = checkInOutMapper.selectRecentByOpenidType(openid, CheckType.CheckIn.ordinal());
+                    if (checkInOut == null){
+                        checkInOut = new CheckInOut();
+                        checkInOut.setOpenid(openid);
+                        checkInOut.setType(CheckType.CheckIn.ordinal());
+                        checkInOut.setTime(new Date());
+                        checkInOutMapper.insert(checkInOut);
+                        //todo return thx to wechat
+                    }else {
+                        Date checkinTime = checkInOut.getTime();
+                        long checkinTimestamp = checkinTime.getTime();
+                        Date current = new Date();
+                        long currentTimestamp = current.getTime();
+                        long duration = currentTimestamp - checkinTimestamp;
+                        if (duration < 5 * 60 * 1000){
+                            logger.info("{} already checkin", openid);
+                            //todo return already checkin notification
+                        }else {
+                            //todo return thx to wechat
+                        }
+                    }
+
 
                 }
             }
@@ -65,6 +101,7 @@ public class WechatMPController {
                 Double longitude = message.getDouble("Longitude");
                 Position position = new Position(latitude, longitude);
                 openidPosition.put(openid,position);
+                logger.info("set user position: {}, {}",openid, JSON.toJSON(position));
             }
 
         }
--- src/main/java/io/cjf/checkinout0516/dao/CheckInOutMapper.java
+++ src/main/java/io/cjf/checkinout0516/dao/CheckInOutMapper.java
@@ -1,6 +1,7 @@
 package io.cjf.checkinout0516.dao;
 
 import io.cjf.checkinout0516.po.CheckInOut;
+import org.apache.ibatis.annotations.Param;
 
 public interface CheckInOutMapper {
     int deleteByPrimaryKey(Integer id);
@@ -12,6 +13,7 @@ public interface CheckInOutMapper {
     CheckInOut selectByPrimaryKey(Integer id);
 
     //todo select by openid and type
+    CheckInOut selectRecentByOpenidType(@Param("openid") String openid, @Param("type") Integer type);
 
     int updateByPrimaryKeySelective(CheckInOut record);
 
