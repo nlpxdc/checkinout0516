bea45d3719f7845e4826a513eeaff7db5318321c
chenjiefei <nlpx_dc@hotmail.com>
2019-05-20 02:06:11 +0800

    add get wechat access token log

--- src/main/java/io/cjf/checkinout0516/service/impl/WechatMPServiceImpl.java
+++ src/main/java/io/cjf/checkinout0516/service/impl/WechatMPServiceImpl.java
@@ -3,12 +3,16 @@ package io.cjf.checkinout0516.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import io.cjf.checkinout0516.api.WechatMPApi;
 import io.cjf.checkinout0516.service.WechatMPService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WechatMPServiceImpl implements WechatMPService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${wechatmp.appId}")
     private String appId;
@@ -21,6 +25,7 @@ public class WechatMPServiceImpl implements WechatMPService {
 
     @Override
     public String getAccessToken() {
+        logger.info("ready to renew wechatmp access token");
         JSONObject jsonObject = wechatMPApi.getAccessToken("client_credential", appId, appSecret);
         String access_token = jsonObject.getString("access_token");
         return access_token;
