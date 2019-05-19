8cc5e24dafecabc35a60d35b923db961e74cad3e
chenjiefei <nlpx_dc@hotmail.com>
2019-05-20 00:41:17 +0800

    fix msg type handler

--- src/main/java/io/cjf/checkinout0516/controller/WechatMPController.java
+++ src/main/java/io/cjf/checkinout0516/controller/WechatMPController.java
@@ -1,10 +1,8 @@
 package io.cjf.checkinout0516.controller;
 
-import io.cjf.checkinout0516.constant.WechatReqMsgTypeConstant;
-import io.cjf.checkinout0516.dto.WechatMPEventReqMsg;
 import io.cjf.checkinout0516.dto.WechatMPReqMsg;
 import io.cjf.checkinout0516.dto.WechatMPResMsg;
-import io.cjf.checkinout0516.handler.EventMsgHandler;
+import io.cjf.checkinout0516.handler.MsgTypeHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +19,7 @@ public class WechatMPController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private EventMsgHandler eventMsgHandler;
-
-    private WechatMPResMsg resMsg;
+    private MsgTypeHandler msgTypeHandler;
 
     @Value("${wechat.signature.verify.enabled}")
     private boolean signatureVerifyEnabled;
@@ -58,44 +54,9 @@ public class WechatMPController {
 
         //todo check async handle
 
-
         logger.info("{}",reqMsg);
 
-        String msgType = reqMsg.getMsgType();
-        String fromUserName = reqMsg.getFromUserName();
-        Integer createTime = reqMsg.getCreateTime();
-
-        switch (msgType) {
-            case WechatReqMsgTypeConstant.TEXT:
-                logger.info("receive {}", WechatReqMsgTypeConstant.TEXT);
-                break;
-            case WechatReqMsgTypeConstant.IMAGE:
-                logger.info("receive {}", WechatReqMsgTypeConstant.IMAGE);
-                break;
-            case WechatReqMsgTypeConstant.VOICE:
-                logger.info("receive {}", WechatReqMsgTypeConstant.VOICE);
-                break;
-            case WechatReqMsgTypeConstant.VIDEO:
-                logger.info("receive {}", WechatReqMsgTypeConstant.VIDEO);
-                break;
-            case WechatReqMsgTypeConstant.SHORT_VIDEO:
-                logger.info("receive {}", WechatReqMsgTypeConstant.SHORT_VIDEO);
-                break;
-            case WechatReqMsgTypeConstant.LOCATION:
-                logger.info("receive {}", WechatReqMsgTypeConstant.LOCATION);
-                break;
-            case WechatReqMsgTypeConstant.LINK:
-                logger.info("receive {}", WechatReqMsgTypeConstant.LINK);
-                break;
-            case WechatReqMsgTypeConstant.EVENT:
-                logger.info("receive {}", WechatReqMsgTypeConstant.EVENT);
-                WechatMPEventReqMsg eventReqMsg = (WechatMPEventReqMsg) reqMsg;
-                WechatMPResMsg resMsg = eventMsgHandler.handle(eventReqMsg);
-                this.resMsg = resMsg;
-                break;
-            default:
-                logger.warn("it doesn't match msg type");
-        }
+        WechatMPResMsg resMsg = msgTypeHandler.handle(reqMsg);
 
         return resMsg;
     }
---
+++ src/main/java/io/cjf/checkinout0516/handler/MsgTypeHandler.java
@@ -0,0 +1,57 @@
+package io.cjf.checkinout0516.handler;
+
+import io.cjf.checkinout0516.constant.WechatReqMsgTypeConstant;
+import io.cjf.checkinout0516.dto.WechatMPEventReqMsg;
+import io.cjf.checkinout0516.dto.WechatMPReqMsg;
+import io.cjf.checkinout0516.dto.WechatMPResMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.constraints.NotBlank;
+
+public class MsgTypeHandler {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private EventMsgHandler eventMsgHandler;
+
+    private WechatMPResMsg resMsg;
+
+    public WechatMPResMsg handle(WechatMPReqMsg reqMsg){
+        @NotBlank String msgType = reqMsg.getMsgType();
+        switch (msgType) {
+            case WechatReqMsgTypeConstant.TEXT:
+                logger.info("receive {}", WechatReqMsgTypeConstant.TEXT);
+                break;
+            case WechatReqMsgTypeConstant.IMAGE:
+                logger.info("receive {}", WechatReqMsgTypeConstant.IMAGE);
+                break;
+            case WechatReqMsgTypeConstant.VOICE:
+                logger.info("receive {}", WechatReqMsgTypeConstant.VOICE);
+                break;
+            case WechatReqMsgTypeConstant.VIDEO:
+                logger.info("receive {}", WechatReqMsgTypeConstant.VIDEO);
+                break;
+            case WechatReqMsgTypeConstant.SHORT_VIDEO:
+                logger.info("receive {}", WechatReqMsgTypeConstant.SHORT_VIDEO);
+                break;
+            case WechatReqMsgTypeConstant.LOCATION:
+                logger.info("receive {}", WechatReqMsgTypeConstant.LOCATION);
+                break;
+            case WechatReqMsgTypeConstant.LINK:
+                logger.info("receive {}", WechatReqMsgTypeConstant.LINK);
+                break;
+            case WechatReqMsgTypeConstant.EVENT:
+                logger.info("receive {}", WechatReqMsgTypeConstant.EVENT);
+                WechatMPEventReqMsg eventReqMsg = (WechatMPEventReqMsg) reqMsg;
+                WechatMPResMsg resMsg = eventMsgHandler.handle(eventReqMsg);
+                this.resMsg = resMsg;
+                break;
+            default:
+                logger.warn("it doesn't match msg type");
+        }
+        return resMsg;
+    }
+}
