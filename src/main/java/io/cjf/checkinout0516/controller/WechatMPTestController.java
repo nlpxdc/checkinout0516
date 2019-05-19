693ee7a617b0ddec863b20864aa60f7f8bbee2b5
chenjiefei <nlpx_dc@hotmail.com>
2019-05-19 13:50:44 +0800

    fix some issue 2

--- src/main/java/io/cjf/checkinout0516/controller/WechatMPTestController.java
+++ src/main/java/io/cjf/checkinout0516/controller/WechatMPTestController.java
@@ -14,7 +14,7 @@ public class WechatMPTestController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private Long count = 0L;
+    private Integer count = 0;
 
     @GetMapping("/receive")
     public String receive(@RequestParam(required = false) String signature,
@@ -32,17 +32,18 @@ public class WechatMPTestController {
 
     @PostMapping(value = "/receive", produces = MediaType.APPLICATION_XML_VALUE)
     public WechatMPResMsg receive(@RequestParam(required = false) String signature,
-                                  @RequestParam(required = false) Long timestamp,
+                                  @RequestParam(required = false) Integer timestamp,
                                   @RequestParam(required = false) String nonce,
-                                  @RequestParam(required = false) String echostr,
                                   @RequestBody(required = false) WechatMPReqMsg reqMsg){
         logger.info("POST Request!!!");
         logger.info("signature: {}", signature);
         logger.info("timestamp: {}", timestamp);
         logger.info("nonce: {}", nonce);
-        logger.info("echostr: {}", echostr);
         logger.info("reqMsg: {}", reqMsg);
 
+        //todo verify with token
+
+        count++;
         TextResMsg textResMsg = new TextResMsg("oUwXe58JsPM6MBFsI3YvnbFIpg-8",count.toString());
         return textResMsg;
     }
