1540127e0abe499fae320bc09623c5165882c6f7
chenjiefei <nlpx_dc@hotmail.com>
2019-05-21 21:59:21 +0800

    fix with exception response with xml

--- src/main/java/io/cjf/checkinout0516/advisor/ExceptionAdvisor.java
+++ src/main/java/io/cjf/checkinout0516/advisor/ExceptionAdvisor.java
@@ -4,6 +4,10 @@ import io.cjf.checkinout0516.dto.TextResMsg;
 import io.cjf.checkinout0516.exception.ClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,9 +17,14 @@ public class ExceptionAdvisor {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler
-    public TextResMsg handleClientException(ClientException ex){
+    public ResponseEntity<TextResMsg> handleClientException(ClientException ex){
         logger.warn("ClientException, Errcode: {}, ErrMsg: {}", ex.getErrCode(), ex.getMessage());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+
         TextResMsg textResMsg = new TextResMsg(ex.getOpenid(), ex.getMessage());
-        return textResMsg;
+        ResponseEntity<TextResMsg> textResMsgResponseEntity = new ResponseEntity<>(textResMsg, headers, HttpStatus.OK);
+        return textResMsgResponseEntity;
     }
 }
